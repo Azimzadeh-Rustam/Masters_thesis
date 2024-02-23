cd3a4371e17e2cbef9485ffef947413f06b28a3c
Rustam <azimzadeh.rustam@gmail.com>
2024-02-23 21:11:36 +0400

    improved some fetures

--- main.py
+++ main.py
@@ -1,41 +1,14 @@
 import numpy as np
+from integral import *
 
 
-class Integral:
-
-    def __init__(self, function=str(), differential=str(), limits=tuple()):
-        if isinstance(function, str) and isinstance(differential, str):
-            self._function = function
-            self._differential = differential
-        self._limits = limits
-
-    def get_func(self):
-        return self._function
-
-    def get_diff(self):
-        return self._differential
-
-    def get_lims(self):
-        return self._limits
-
-    def set_func(self, function):
-        if isinstance(function, str):
-            self._function = function
-        else:
-            raise ValueError("The function must be a string")
-
-    def set_diff(self, differential):
-        if isinstance(differential, str):
-            self._differential = differential
-        else:
-            raise ValueError("The differential must be a string")
-
-    def set_lims(self, limits):
-        self._limits = limits
+def main():
+    function = r"x^2"
+    differential = r"dx"
+    limits = (2, -2)
+    loop_integral = Integral(function, differential, limits)
+    print(loop_integral.__dict__)
 
 
-function = r"x^2"
-differential = r"dx"
-limits = (2, -2)
-loopIntegral = Integral(function, differential, limits)
-print(loopIntegral.__dict__)
+if __name__ == '__main__':
+    main()
