0832c9f93ad60bb0ae427d15351e87811152245d
Rustam <azimzadeh.rustam@gmail.com>
2025-03-23 22:10:11 +0400

    corrected function that decomposes expressions into hyperspherical functions

--- math_objects/simplifier.py
+++ math_objects/simplifier.py
@@ -1,50 +1,32 @@
 import re
 from typing import Tuple
 
+from torch.distributed import group
+
+
 class Simplifier:
     def __init__(self):
         pass
 
-    #def simplify(self, input_string: str) -> str:
-    #    input_string = self.clean_string(input_string)
-    #
-    #    while True:
-    #        match = self.find_polynomial_expression(input_string)
-    #        if not match:
-    #            break
-    #        input_string = self.expand_polynomial(input_string, match)
-    #        print(f'Expanded expression: {input_string}')
-    #
-    #    terms = self.split_terms(input_string)
-    #    terms = [self.set_limits(term.strip()) for term in terms]
-    #    input_string = "+".join(terms)
-    #    return input_string
+    largest_pulse = 'Q'
 
     def simplify(self, input_string: str) -> str:
         input_string = self.clean_string(input_string)
-        print('Input:     ' + input_string)
-        input_string = self.simplify_fractions(input_string)
-        print('Fractions: ' + input_string)
+        print(f'Input         : {input_string}')
+        input_string = self.decompose_pulses(input_string)
+        print(f'Decompose     : {input_string}')
+        input_string = self.extract_constants_fractions(input_string)
+        print(f'Fractions     : {input_string}')
+        input_string = self.combine_constants(input_string)
+        print(f'Constants     : {input_string}')
         input_string = self.apply_gaussian_integration(input_string)
-        print('Gauss:     ' + input_string)
-        terms = [input_string]
-        while any(re.search(r'\(([A-Z])-([A-Z])\)\^2', term) for term in terms):
-            expanded_terms = []
-            for term in terms:
-                if match := re.search(r'\(([A-Z])-([A-Z])\)\^2', term):
-                    pulse1, pulse2 = match.groups()
-                    expanded_terms.append(term.replace(match.group(0),
-                                                       f'F{{1}}{{{pulse1}^2}}*SUM{{(F{{{pulse2}}}{{{pulse1}}})^n*C_n({pulse1 + pulse2})}}',
-                                                       1))
-                    expanded_terms.append(term.replace(match.group(0),
-                                                       f'F{{1}}{{{pulse2}^2}}*SUM{{(F{{{pulse1}}}{{{pulse2}}})^n*C_n({pulse1 + pulse2})}}',
-                                                       1))
-                else:
-                    expanded_terms.append(term)
-            terms = expanded_terms
+        print(f'Gauss         : {input_string}')
+        terms = self.expand_into_hyperspherical_functions(input_string)
+        print(f'Hyperspherical: {"+".join(terms)}')
+        #terms = [self.redistribute_terms_to_integrals(term) for term in terms]
         terms = [self.set_limits(term) for term in terms]
-        input_string = "+".join(terms)
-        print('Decompose: ' + input_string)
+        terms = [term for term in terms if term] # remove empty string elements
+        print(f'Limits        : {"+".join(terms)}')
         return input_string
 
 
@@ -52,60 +34,128 @@ class Simplifier:
         return "".join(input_string.split())
 
 
-    def separate_constants(self, factors: list[str]) -> Tuple[list[str], list[str]]:
-        """
-        Разделяет множители на числовые/константные и переменные.
-        """
-        constants = []
-        variables = []
+    def separate_constants(self,expression: str) -> tuple[str, str]:
+        constants_pattern = re.compile(
+            r'PI'  # "PI"
+            r'|\(2\*PI\)\^\d+'  # "(2*PI)^число"
+            r'|(?<!\^)\d+'  # просто число, которое не является степенью какой-то другой переменной или выражения
+        )
+        constants = constants_pattern.findall(expression)
+        expression_constants = '*'.join(constants)
 
-        for factor in factors:
-            if re.match(r'^[0-9PI\^\(\)]+$', factor):  # Константы типа '2', 'PI', '2*PI^4'
-                constants.append(factor)
-            else:  # Остальные множители считаются переменными
-                variables.append(factor)
-        return constants, variables
+        cleaned_expression = re.sub(constants_pattern, '', expression)
+        # Убираем лишние символы умножения: если * повторяется два или более раз
+        cleaned_expression = re.sub(r'\*+', '*', cleaned_expression)
+        # Убираем * в начале и в конце строки
+        cleaned_expression = cleaned_expression.strip('*')
 
+        return cleaned_expression, expression_constants
 
-    def simplify_fractions(self, input_string: str) -> str:
-        gauss_integral_pattern = re.compile(r'INT#\{F\{(.*)(dS\^\{[A-Z]\}_\{mu\})([^}]*)\}\{([^}]+)\}\}')
-        gauss_integral_match = gauss_integral_pattern.search(input_string)
+    def extract_constants_fractions(self, input_string: str) -> str:
+        def fraction_replacer(fraction_match: re.Match) -> str:
+            numerator, denominator = fraction_match.group(1), fraction_match.group(2)
+            cleaned_numerator, numerator_consts = self.separate_constants(numerator)
+            cleaned_denominator, denominator_consts = self.separate_constants(denominator)
 
-        numerator_multiplier1 = gauss_integral_match.group(1)
-        numerator_multiplier2 = gauss_integral_match.group(3)
-        denominator = gauss_integral_match.group(4)
+            if not numerator_consts and not denominator_consts:
+                return fraction_match.group(0)  # Возвращаем оригинальную дробь без изменений
 
-        if numerator_multiplier1 or numerator_multiplier2 or denominator:
-            fraction = 'F{' + numerator_multiplier1 + numerator_multiplier2 + '}{' + denominator + '}'
-            modified_expression = 'INT#{' + gauss_integral_match.group(2) + '}'
-            input_string = gauss_integral_pattern.sub(modified_expression, input_string)
-            input_string = f'{fraction}*{input_string}'
+            # Формирование замены для дроби без констант
+            fraction_replacement = ''
+            if cleaned_numerator and cleaned_denominator:
+                fraction_replacement = f'F{{{cleaned_numerator}}}{{{cleaned_denominator}}}'
+            elif cleaned_numerator:
+                fraction_replacement = f'{cleaned_numerator}'
+            elif cleaned_denominator:
+                fraction_replacement = f'F{{}}{{{cleaned_denominator}}}'
+
+            # Формирование выражения с константами
+            constants_expression = ''
+            if numerator_consts and denominator_consts:
+                constants_expression += f'F{{{numerator_consts}}}{{{denominator_consts}}}'
+            elif numerator_consts:
+                constants_expression += f'{numerator_consts}'
+            elif denominator_consts:
+                constants_expression += f'F{{}}{{{denominator_consts}}}'
+
+            # Заменяем старую дробь на обработанную
+            replacement = f'{constants_expression}*{fraction_replacement}'.rstrip('*')
+            return replacement
+
+        # Unify extra constants in a single factors from the entire expression. Применение рекурсивной обработки дробей ко всему выражению
+        fraction_pattern = re.compile(r'F\{(.*?)\}\{(.*?)\}')
+        input_string = fraction_pattern.sub(fraction_replacer, input_string)
+
         return input_string
-        #def process_fraction(match: re.Match) -> str:
-        #    numerator, denominator = match.groups()
-        #
-        #    # Разделяем числитель и знаменатель на множители
-        #    numerator_multipliers = numerator.split('*')
-        #    denominator_multipliers = denominator.split('*')
-        #
-        #    # Фильтруем числовые и буквенные множители
-        #    numerator_constants, numerator_variables = self.separate_constants(numerator_multipliers)
-        #    denominator_constants, denominator_variables = self.separate_constants(denominator_multipliers)
-        #
-        #    # Формируем обновленную строку с вынесенными множителями
-        #    const_part = f"F{{{''.join(numerator_constants)}}}{{{''.join(denominator_constants)}}}" if numerator_constants or denominator_constants else ""
-        #    var_part = f"F{{{'*'.join(numerator_variables)}}}{{{'*'.join(denominator_variables)}}}" if numerator_variables or denominator_variables else ""
-        #
-        #    # Формируем итоговую строку
-        #    if const_part and var_part:
-        #        return f"{const_part}*{var_part}"
-        #    return const_part or var_part
-        #
-        #fraction_pattern = re.compile(r'F\{([^}]*)\}\{([^}]*)\}')
 
-        # Замена всех найденных дробей с использованием process_fraction
-        #result = fraction_pattern.sub(process_fraction, input_string)
-        #return result
+
+    def combine_constants(self, input_string: str) -> str:
+        combined_numerator = ''
+        combined_denominator = ''
+        combined_fraction = ''
+        ######################## COMBINE CONSTANTS FROM FRACTIONS ##########################
+        fraction_pattern = re.compile(r'F\{(.*?)\}\{(.*?)\}')
+        fractions = fraction_pattern.findall(input_string)
+
+        for fraction in fractions:
+            numerator, denominator = fraction
+            cleaned_numerator, numerator_consts = self.separate_constants(numerator)
+            cleaned_denominator, denominator_consts = self.separate_constants(denominator)
+
+            # Проверяем, состоит ли дробь только из констант
+            if not cleaned_numerator and not cleaned_denominator:
+                # Создаем общий числитель
+                if not combined_numerator and numerator_consts:
+                    combined_numerator = numerator_consts
+                elif combined_numerator and numerator_consts:
+                    combined_numerator += f'*{numerator_consts}'
+
+                # Создаем общий знаменатель
+                if not combined_denominator and denominator_consts:
+                    combined_denominator = denominator_consts
+                elif combined_denominator and denominator_consts:
+                    combined_denominator += f'*{denominator_consts}'
+
+                # Удаление дроби и очистка лишних символов '*'
+                pattern = rf'(\*?)F\{{{re.escape(numerator)}}}\{{{re.escape(denominator)}}}(\*?)'
+                matches = re.findall(pattern, input_string)
+                for left_star, right_star in matches:
+                    if left_star and right_star:  # Дробь окружена '*'
+                        input_string = re.sub(pattern, '*', input_string, count=1)
+                    elif left_star or right_star:  # Либо слева, либо справа есть '*'
+                        input_string = re.sub(pattern, '', input_string, count=1)
+                    else:  # Обычное удаление без лишних '*'
+                        input_string = re.sub(pattern, '', input_string, count=1)
+
+        combined_numerator = combined_numerator.strip('*')
+        combined_denominator = combined_denominator.strip('*')
+        ######################## COMBINE CONSTANTS ################################
+
+        ######################## CREATE FINAL EXPRESSION WITH CONSTANTS ################################
+        if combined_numerator and combined_denominator:
+            combined_fraction = f'F{{{combined_numerator}}}{{{combined_denominator}}}'
+        elif combined_numerator and not combined_denominator:
+            combined_fraction = f'{combined_numerator}'
+        elif not combined_numerator and combined_denominator:
+            combined_fraction = f'F{{}}{{{combined_denominator}}}'
+        ######################## PUSHING CONSTANTS TO THE BEGINNING OF THE EXPRESSION #########################
+        input_string = f'{combined_fraction}*{input_string}'
+        return input_string
+
+
+    def decompose_pulses(self, input_string: str) -> str:
+        pattern = re.compile(r'd\^4\{([A-Z])\}')
+
+        def replace_pulse(match):
+            pulse = match.group(1)
+
+            if pulse == 'G':  # Исключаем G (он зарезервирован под омегу)
+                return match.group(0)
+            return f'd{pulse}*{pulse}^3*INT{{dG_{pulse}}}'
+
+        # Замена найденных выражений
+        input_string = pattern.sub(replace_pulse, input_string)
+        return input_string
 
 
     def apply_gaussian_integration(self, input_string: str) -> str:
@@ -128,73 +178,33 @@ class Simplifier:
         return input_string
 
 
-        #if match:
-            #result = f'2*PI^2*{pulse}^4'
-            #input_string = integral_match.sub(modified_integral, input_string, count=1)
-        #    input_string = pattern.sub('', input_string, count=1)  # Удаляем выражение из строки
-        #    input_string = result + '*' + input_string  # Переносим в начало
-        #return input_string
+    def expand_into_hyperspherical_functions(self, input_string):
+        terms = [input_string]
+        indices = ['n', 'l', 'm', 'p', 'q', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        index_number = 0
+        while any(re.search(r'F\{.*?\}\{.*?\(([A-Z])-([A-Z])\)\^2.*?\}', term) for term in terms):
+            expanded_terms = []
+            for term in terms:
+                if match := re.search(r'F\{(.*?)\}\{(.*?)\*?\(([A-Z])-([A-Z])\)\^2\*?(.*?)\}', term):
+                    numerator, part1, pulse1, pulse2, part2 = match.groups()
+                    denominator = ''
+                    if part1 and part2:
+                        denominator = f'{part1}*{part2}'
+                    if part1 and not part2:
+                        denominator = part1
+                    if not part1 and part2:
+                        denominator = part2
 
-    def find_polynomial_expression(self, input_string: str):
-        pattern = re.compile(r'\(([A-Z])-([A-Z])\)\^2')
-        return pattern.search(input_string)
+                    index = indices[index_number]
 
-    #def find_the_last_polynomial(self, input_string: str) -> str:
-    #    search_pattern = re.compile(
-    #        r'.*F\{.*\}\{.*(\(([A-Z])-([A-Z])\)\^2)\}.*'
-    #    )
-    #
-    #    match = re.search(search_pattern, input_string)
-    #    polinom = match.group(1)
-    #    print(polinom)
-    #    pulse1 = match.group(2)
-    #    print(pulse1)
-    #    pulse2 = match.group(3)
-    #    print(pulse2)
+                    expanded_terms.append(term.replace(match.group(0), f'F{{{numerator}}}{{{denominator}}}*F{{}}{{{pulse1}^2}}*SUM{{(F{{{pulse2}}}{{{pulse1}}})^{index}*C_{index}({pulse1 + pulse2})}}', 1))
+                    expanded_terms.append(term.replace(match.group(0), f'F{{{numerator}}}{{{denominator}}}*F{{}}{{{pulse2}^2}}*SUM{{(F{{{pulse1}}}{{{pulse2}}})^{index}*C_{index}({pulse1 + pulse2})}}', 1))
+                else:
+                    expanded_terms.append(term)
+            index_number += 1
+            terms = expanded_terms
+        return terms
 
-    def expand_polynomial(self, input_string: str, match) -> str:
-        pulse1, pulse2 = match.groups()
-
-        first_term = f'F{{1}}{{{pulse1}^2}} * SUM{{(F{{{pulse2}}}{{{pulse1}}})^n * C_n({pulse1 + pulse2})}}'
-        second_term = f'F{{1}}{{{pulse2}^2}} * SUM{{(F{{{pulse1}}}{{{pulse2}}})^n * C_n({pulse1 + pulse2})}}'
-
-        expanded_expression = first_term + ' + ' + second_term
-
-        return input_string[:match.start()] + expanded_expression + input_string[match.end():]
-
-    #def expand_into_polynomials(self, input_string: str) -> str:
-    #    search_pattern = re.compile(
-    #        r'.*F\{.*\}\{.*(\(([A-Z])-([A-Z])\)\^2)\}.*'
-    #    )
-    #
-    #    match = re.search(search_pattern, input_string)
-    #    if not match:
-    #        raise ValueError("Не удалось найти подходящее выражение в строке.")
-    #
-    #    full_expression = match.group(1)
-    #    pulse1 = match.group(2)
-    #    pulse2 = match.group(3)
-    #
-    #    first_member = input_string.replace(full_expression, f'F{{1}}{{{pulse1}^2}}*SUM{{(F{{{pulse2}}}{{{pulse1}}})^n*C_n({pulse1 + pulse2})}}')
-    #    second_member = input_string.replace(full_expression, f'F{{1}}{{{pulse2}^2}}*SUM{{(F{{{pulse1}}}{{{pulse2}}})^n*C_n({pulse1 + pulse2})}}')
-    #
-    #    modified_string = first_member + '+' + second_member
-    #    return modified_string
-
-    def find_polinoms(self, expression: str) -> str:
-        search_pattern = r"INT\(C_(\d+)\(([A-Z][A-Z])\)C_(\d+)\(([A-Z][A-Z])\)domega\)"
-
-        # Поиск всех совпадений
-        matches = re.findall(search_pattern, expression)
-
-        if matches:
-            print("Найденные совпадения:")
-            for match in matches:
-                print(match)  # Выводит кортежи групп, например: ('1', 'QL', '2', 'LQ')
-        else:
-            pass # Придумать что-то
-
-        return expression
 
     def simplify_polinom(self, expression: str) -> str:
         search_pattern = re.compile(
@@ -215,17 +225,62 @@ class Simplifier:
 
         return re.sub(search_pattern, create_replacement, expression)
 
+
     def split_terms(self, input_string: str):
         return input_string.split("+")
 
-    def define_limits(self, input_string: str):
-        pass
+
+    def redistribute_terms_to_integrals(self, input_string: str):
+        diff_var = 'K'
+        integral_search_pattern = re.compile(
+            rf'INT{{(.*)d{diff_var}\*{diff_var}\^3\*INT{{dG_{diff_var}}}(.*)}}'
+        )
+        match = re.search(integral_search_pattern, input_string)
+        part1, part2 = match.groups()
+        print(part1)
+        print(part2)
+        return input_string
+
 
     def set_limits(self, input_string: str):
-        search_pattern = re.compile(r'\bINT\b')
-        upper_limit = 'b'
-        lower_limit = 'a'
+        pulse_sequence = list()
+        pulse_sequence.append(self.largest_pulse)
 
-        definite_integral = f'INT_{lower_limit}^{upper_limit}'
-        modified_string = re.sub(search_pattern, definite_integral, input_string)
-        return modified_string
+        pattern = re.compile(r'\(F\{([A-Z])\}\{([A-Z])\}\)\^[a-z]')
+        pair_pulses = pattern.findall(input_string)
+
+        for numerator, denominator in pair_pulses:
+            if numerator == self.largest_pulse:
+                return '' # Such expression cannot exist
+
+            if denominator == self.largest_pulse:
+                continue
+
+            if denominator not in pulse_sequence:
+                pulse_sequence.append(denominator)
+                pulse_sequence.append(numerator)
+
+        ############### FIRST PULSE
+        diff_pulse = pulse_sequence[2]
+        integral_search_pattern = re.compile(
+            rf'INT{{(.*?)d{diff_pulse}(.*)}}'
+        )
+        match = re.search(integral_search_pattern, input_string)
+        if match:
+            upper_limit = pulse_sequence[1]
+            lower_limit = '0'
+            definite_integral = f'INT_{lower_limit}^{upper_limit}{{{match.group(1)}d{diff_pulse}{match.group(2)}}}'
+            input_string = re.sub(integral_search_pattern, definite_integral, input_string)
+
+        ############### SECOND PULSE
+        diff_pulse = pulse_sequence[1]
+        integral_search_pattern = re.compile(
+            rf'INT{{(.*)d{diff_pulse}(.*)}}'
+        )
+        if match:
+            upper_limit = pulse_sequence[0]
+            lower_limit = '0'
+            definite_integral = f'INT_{lower_limit}^{upper_limit}{{{match.group(1)}d{diff_pulse}{match.group(2)}}}'
+            input_string = re.sub(integral_search_pattern, definite_integral, input_string)
+
+        return input_string
